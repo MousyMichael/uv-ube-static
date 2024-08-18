dc5b28facc690b8140eb3eba49cd80512dc1227d
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-08-18 15:35:22 -0400

    Update ube-manager.js

--- public/ube-manager.js
+++ public/ube-manager.js
@@ -1,1 +1,18 @@
+console.log("Modified for UnblockedEverything's Search feature.")
+console.log("Powered By Ultraviolet - TN")
 
+//
+
+const sp = new URLSearchParams(window.location.search)
+
+const engine = sp.get("e")
+const search = sp.get("s")
+
+const engineInput = document.getElementById("uv-search-engine")
+const searchInput = document.getElementById("uv-address")
+const form = document.getElementById("uv-form")
+
+engineInput.setAttribute("value", engine)
+searchInput.setAttribute("value", search)
+
+form.submit()
